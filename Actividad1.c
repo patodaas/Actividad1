eafd7c0d6bdeb74c20299ea54af0962e7c3a8a2e
patodaas <patricio.davila@edu.uag.mx>
2025-08-28 17:17:25 +0000

    Código añadido

---
+++ Actividad1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	printf("¿Cuántas veces quieres que mi nombre se repita?");
+	scanf("%d",&n);
+	for(int i=0; i<n; i++){
+		printf("Patricio Dávila Assad");
+	}
+	return 0;
+}
+
